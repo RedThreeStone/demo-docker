d3c311ebd755caac57d224516cbc9589b1443cc9
RedThreeStone <42318909+RedThreeStone@users.noreply.github.com>
2019-11-15 20:45:33 +0800

    Initial commit

---
+++ src/test/java/it/lei/demodocker/DemoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lei.demodocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
